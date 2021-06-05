2dac0fb8daeaeda2f326da7e13554117f6de00a2
nino <nino.parolari@gmail.com>
2021-06-05 16:19:43 +0200

    windows sind verschiebbar

--- pages/start/start.js
+++ pages/start/start.js
@@ -1,9 +1,4 @@
 $( document ).ready(function() {
-  // $('.video-container video').bind('ended', function(){
-  //    $(this).parent().addClass('fadeout');
-  //    setTimeout(function(){
-  //      $('.video-container').addClass('hidden');
-  //    }, 1100);
      setTimeout(function(){
        $('.content-container').removeClass('hidden');
      }, 1200);
@@ -16,17 +11,13 @@ $( document ).ready(function() {
      setTimeout(function(){
        $('.window').addClass('fadein');
      }, 4000);
-     //setTimeout(function(){
-    //   $('h1').text('Immer diese mühsamen Antivirus-Updates.. Soll Horst es ausführen?')
-    // }, 5000);
-  
-
-  $('.update-now').on('click', function() {
-    sessionStorage.setItem("update", "yes");
-    window.location.href = "../update/update.html";
-  });
-  $('.update-later').on('click', function() {
-    sessionStorage.setItem("update", "no");
-    window.location.href = "../update/update.html";
-  });
+      $('.update-now').on('click', function() {
+        sessionStorage.setItem("update", "yes");
+        window.location.href = "../update/update.html";
+      });
+      $('.update-later').on('click', function() {
+        sessionStorage.setItem("update", "no");
+        window.location.href = "../update/update.html";
+      });
 });
+
